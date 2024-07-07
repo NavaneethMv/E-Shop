5e637fe6a50429a9d88e236d8d3448e253ece44f
MVNavaneeth <mvnavaneeth5@gmail.com>
2024-07-07 07:07:15 +0530

    added new pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,12 @@
 import 'package:get/get.dart';
+import 'package:eshop/app/modules/auth/bindings/auth_binding.dart';
+import 'package:eshop/app/modules/auth/views/otp_view.dart';
 import 'package:eshop/app/modules/onbording/views/onbording_view.dart';
 import 'package:eshop/app/modules/onbording/bindings/onbording_bindings.dart';
 import 'package:eshop/app/routes/app_routes.dart';
+import 'package:eshop/app/modules/auth/views/auth_view.dart';
+import 'package:eshop/app/modules/home/views/home_view.dart';
+import 'package:eshop/app/modules/home/bindings/home_bindings.dart';
 
 class AppPages {
   static const INITIAL = Routes.ONBORDING;
@@ -11,6 +16,21 @@ class AppPages {
       name: Routes.ONBORDING,
       page: () => OnBordingView(),
       binding: OnBordingBinding(),
+    ),
+    GetPage(
+      name: Routes.AUTH,
+      page: () => AuthView(),
+      binding: AuthBinding(),
+    ),
+    GetPage(
+      name: Routes.OTP,
+      page: () => OtpView(),
+      binding: AuthBinding(),
+    ),
+    GetPage(
+      name: Routes.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
     ),
   ];
 }
