d0ac9a81a0d2e8191897308b09d75ebbfdfdd68b
MVNavaneeth <mvnavaneeth5@gmail.com>
2024-07-07 07:06:02 +0530

    Changed route

--- lib/app/modules/onbording/controllers/onbording_controller.dart
+++ lib/app/modules/onbording/controllers/onbording_controller.dart
@@ -3,6 +3,6 @@ import 'package:get/get.dart';
 
 class OnBordingController extends GetxController {
   void navigateToLogin() {
-    Get.offAllNamed(Routes.ONBORDING);
+    Get.offAllNamed(Routes.AUTH);
   }
 }
